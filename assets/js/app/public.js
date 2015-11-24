fe90eedde1f08d3f46afabe9a56dd5367148b6c4
Scott Stadt <scott.stadt@gmail.com>
2015-11-23 20:43:14 -0600

    Removed the old landing page and restyle the parallax landing page for touch screens

--- assets/js/app/public.js
+++ assets/js/app/public.js
@@ -2,12 +2,14 @@
 
 require([
   'jquery',
+  'modernizr',
   'parallax',
   'lib/global'
 ], function ($) {
   'use strict';
 
   $('.parallax').imageScroll({
-    coverRatio: 0.9
+    coverRatio: 0.9,
+    touch: Modernizr.touch
   });
 });
